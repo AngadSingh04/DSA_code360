8afb4ea77d649433b62ca205e92eb525de12352a
AngadSingh04 <singh04angad@gmail.com>
2025-04-04 11:13:10 +0530

    add binary search implementation using both loops and recursion

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,3 +1,5 @@
+// using loops
+
 int search(vector<int> &nums, int target) {
     // Write your code here.
     int l = 0;
@@ -15,4 +17,35 @@ int search(vector<int> &nums, int target) {
     }
 
     return -1;
+}
+
+// Using recursion
+
+
+int isBinarySearch(vector<int> &arr, int l, int h, int key){
+    int mid = l + (h-l)/2;
+
+    if(l > h){
+        return -1;
+    }
+    if(arr[mid] == key){
+        return mid;
+    }else{
+        if(arr[mid] > key){
+            return isBinarySearch(arr, l, mid-1, key);
+        }else{
+            return isBinarySearch(arr,mid+1, h, key);
+        }
+    }
+}
+
+int search(vector<int> &nums, int target) {
+    // Write your code here.
+
+    int l = 0;
+    int h = nums.size()-1;
+
+    int ans = isBinarySearch(nums, l, h, target);
+    return ans;
+   
 }
